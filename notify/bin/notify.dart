ee05f5f9e870ae3a801abce3d7953cbecd697ea6
Shaheer Mirza <shaheermirzacs@gmail.com>
2021-06-17 04:59:09 -0500

    added notify and updated github action to produce notify.exe

---
+++ notify/bin/notify.dart
@@ -0,0 +1,5 @@
+import 'package:notify/notify.dart' as notify;
+
+void main(List<String> arguments) {
+  notify.main(arguments);
+}
---
+++ notify/test/notify_test.dart
@@ -0,0 +1,8 @@
+import 'package:notify/notify.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
